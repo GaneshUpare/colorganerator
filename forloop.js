d17fe32dcf34a3d4ab193a5a4ba5aac79b7c6b8e
Ganesh Upare <ganeshupare3108@gmail.com>
2024-03-05 00:17:52 +0530

    first commit

---
+++ forloop.js
@@ -0,0 +1,48 @@
+// // var sum=0;
+// // debugger
+// // for (var num=1; num<=10; num++)
+// // {
+// //     sum=sum+num;
+// // }
+// // console.log(sum)
+
+// var num =prompt("Enter the number");
+// num=parseInt(num);
+// var isprime=true;
+
+// for (var i=2;i<num;i++)
+// {
+//     if(num%i===0){
+//           isprime=false;
+//           break;
+//     }
+// }
+// if(isprime){
+//     console.log("numer is prime");
+// }
+// else{
+//     console.log("numer is not prime");
+// }
+
+
+// for (var i=0;i<=5;i++)
+// {
+//     var pattern="";
+//       for(var j=0;j<=i;j++)
+//     {
+//         pattern=pattern + " *";
+//     }
+//     console.log(pattern);
+// }
+
+
+for (var i=5;i<=0;i--)
+{
+    var pattern="";
+      for(var j=5;j<=i;j--)
+    {
+        pattern=pattern + " *";
+    }
+    console.log(pattern);
+
+}
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+let text="Ganesh Upare"
+//console.log(text.length)
+
+let array=Array.from(text)
+console.log(array)
+
+
